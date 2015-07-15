1b700a372386fa97d2712b8f38d403f3f25b94b3
Titus Jung <titusjung@gmail.com>
2015-07-15 15:50:52 -0700

    working player system

--- assignment2/src/assignment2/Assign.java
+++ assignment2/src/assignment2/Assign.java
@@ -1,5 +1,9 @@
 package assignment2;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
@@ -34,22 +38,40 @@ public class Assign {
 			}
 			//store players
 			bf = new BufferedReader(fl);
-			String [] list = new String[10];
+			try
+			{
+				bf.readLine();
+			}
+			catch(IOException ioe)
+			{
+				System.out.println("IOException: " + ioe.getMessage());
+			}
+			List<Player> plist = new ArrayList();
+			String caught;
 			for(int i=0; i<10;i++)
 			{
 				try
 				{
-				matcher = pattern.matcher(bf.readLine());				
-				list[i]=matcher.group(1);
+					caught = bf.readLine();
+					System.out.println(caught);
+					matcher = pattern.matcher(caught);
+					if(matcher.matches())
+						plist.add(new Player(matcher.group(1), Integer.parseInt(matcher.group(2))));
+					else break;
 				}
 				catch(IOException ioe)
 				{
 					System.out.println("IOException: " + ioe.getMessage());
 				}
 			}
-			for(int i=0; i<10;i++)
+			for(Player p: plist)
 			{
-				System.out.println(list[i]);
+				System.out.println(p.getName()+" "+p.getScore());
+			}
+			Collections.sort(plist, new Player());
+			for(Player p: plist)
+			{
+				System.out.println(p.getName()+" "+p.getScore());
 			}
 			//create class of players?
 			//store it in a 2d array;
@@ -57,4 +79,40 @@ public class Assign {
 			input.close();
 		}
 }
+
+class Player implements Comparator<Player>, Comparable<Player> 
+{
+	private String Name;
+	private int Score;
+	private static int numberPlayers =0;
+	public Player(){}
+	public Player(String n, int s)
+	{
+		Name = n;
+		Score = s; 
+		numberPlayers++;
+	}
+	public String getName()
+	{
+		return Name;
+	}
+	public int getScore()
+	{
+		return Score;
+	}
+	@Override
+	public int compareTo(Player arg0) {
+		// TODO Auto-generated method stub
+		return this.Name.compareTo(arg0.getName());
+	}
+	public static int getNumPlayers()
+	{
+		return numberPlayers;
+	}
+	@Override
+	public int compare(Player arg0, Player arg1) {
+		return arg1.getScore()-arg0.getScore();
+	}
+}
+
 
---
+++ regextest/src/regextest/testregex.java
@@ -0,0 +1,14 @@
+package regextest;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class testregex {
+	public static void regextest(String [] args)
+	{
+	Pattern pattern = Pattern.compile("\\d+\\. (Player\\d) - (\\d+)");
+	Matcher matcher;
+
+	}
+}
